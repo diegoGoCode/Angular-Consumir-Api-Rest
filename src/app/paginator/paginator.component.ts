9da87db9484c74b0c8ad31626482a09431ad782a
DiegoGoCode <diegoalejandrodiazpaez@gamil.com>
2020-06-02 13:18:25 -0500

    Se agrego el componente para el paginado de los clientes

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
   @Input() paginador:any;
   paginas: number[];
+
+  desde: number;
+  hasta: number;
   constructor() { }
 
   ngOnInit(): void {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
+    this.initPaginator();
+  } 
+
+  ngOnChanges(changes: SimpleChanges){
+    
   }
 
+  private initPaginator(): void{
+    this.desde = Math.min(Math.max(1, this.paginador.number -4), this.paginador.totalPages -5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number +4), 6);
+
+    if(this.paginador.totalPages > 5){
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((valor, indice) => indice + this.desde);
+    }else{ 
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
+    }
+  }
 }
